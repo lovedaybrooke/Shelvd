b9b9f68ff4cfaf1bd94fa98a5bc721e45ad560dc
LovedayBrooke <katmatfield@gmail.com>
2013-09-11 21:34:14 +0100

    Grammar, with an add-to-reading-list code added

---
+++ shelvd/grammar.py
@@ -0,0 +1,21 @@
+from pyparsing import *
+
+# Adam sez that this line means the only thing imported when I do
+# 'from grammar import *' will be the value of expression.
+__all__ = ['expression']
+
+initiator = (Keyword('begin') | Keyword('start')).setResultsName("initiator")
+terminator = (Keyword('end') | Keyword('finish')
+    | Keyword('abandon')).setResultsName("terminator")
+readinglist = (Keyword('addtoreadinglist')).setResultsName("readinglist")
+ISBN = Word(nums, min=10, max=13).setResultsName("ISBN")
+nick = Word(alphas).setResultsName("nick")
+percent = (Word(nums, min=1, max=2) + Word("%",
+    exact=1)).setResultsName("percent")
+page = Word(nums, min=1, max=4).setResultsName("page")
+
+
+identifier = ISBN | nick
+command = initiator | terminator | readinglist | percent | page
+
+expression = identifier + command | ISBN + nick
