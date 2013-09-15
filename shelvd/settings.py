da297cb1bc300e524716c94b46bde929b27c4809
LovedayBrooke <katmatfield@gmail.com>
2013-09-15 12:55:57 +0100

    Basic templates to start off

--- shelvd/settings.py
+++ shelvd/settings.py
@@ -107,10 +107,11 @@ ROOT_URLCONF = 'shelvd.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'shelvd.wsgi.application'
 
+import os.path
+PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, "templates"),
+    # here you can add another templates directory if you wish.
 )
 
 INSTALLED_APPS = (
