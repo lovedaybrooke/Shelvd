2e51a947fd9d9e8eb176aed2b12c70f8d2c4a486
LovedayBrooke <katmatfield@gmail.com>
2013-09-15 15:42:46 +0100

    Add shelvd to installed apps

--- shelvd/settings.py
+++ shelvd/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shelvd',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
